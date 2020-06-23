bb7010e8e75eedf0f79356042507c70137590604
Gabriel Brammer <gbrammer@gmail.com>
2020-06-23 10:35:57 +0200

    fig1 with image 4

--- Analysis/mrg/fig1.py
+++ Analysis/mrg/fig1.py
@@ -67,10 +67,12 @@ def rgb():
     N, inter = 10, 0.5
     
     N, inter = 20, 1.0
+
+    xy0 = np.array(wcs.all_world2pix(coo['ra'], coo['dec'], 0)).T
     
     xy = np.cast[int](np.round(wcs.all_world2pix(coo['ra'], coo['dec'], 0))).T
     
-    for i in [4,5,6]:
+    for i in [4,5,6,7]:
         
         xi, yi = xy[i]
         slx = slice(xi-N, xi+N)
@@ -85,36 +87,60 @@ def rgb():
     from matplotlib.gridspec import GridSpec
     
     # Label offsets
-    dx = [-30, 10, 0, -10, 0, 0, -38]
-    dy = [-8, 14, 10, 5, 10, 10, -15]
+    dx = [-30, 10, 0, -28, 0, 0, -38, -27]
+    dy = [-8, 14, 10, 0, 10, 10, -15, 10]
+        
+    ny = 3
+    ny = 4
     
-    fig = plt.figure(figsize=(8,3./5*8))
-    gs = GridSpec(3, 5, figure=fig)
+    if ny == 3:
+        fig = plt.figure(figsize=(8,3/5.*8))
+    else:
+        fig = plt.figure(figsize=(8,4/6.*8))
+        
+    gs = GridSpec(ny, 2+ny, figure=fig)
     
-    ax = fig.add_subplot(gs[:,:3])
+    ax = fig.add_subplot(gs[:,:ny])
     #ax.plot([0,1])
     img = plt.imread('j013804m2156.on.png')
     sh = img.shape
     xyi = (xy - np.array([x0-NX, y0-NY]))/np.array([NX, NY])/2*np.array(sh[:-1])
+    xyi0 = (xy0 - np.array([x0-NX, y0-NY]))/np.array([NX, NY])/2*np.array(sh[:-1])
     #dy = 20
+    pixgrow = sh[0]/2/NX
     
     ax.imshow(img, origin='upper')
     ax.axis('off')
     ax.text(0.03, 0.97, 'a)', ha='left', va='top', transform=ax.transAxes, color='w')
     
-    for j, i in enumerate(range(7)):#[4,5,6]):            
-        ax.text(xyi[i][0]+dx[i], sh[0]-xyi[i][1]+dy[i], coo['label'][i], ha='center', va='top', color='w', fontsize=8)
+    for j, i in enumerate(range(8)):#[4,5,6]):            
+        if i == 7:
+            col = 'pink'
+        else:
+            col = 'w'
+        
+        ax.text(xyi[i][0]+dx[i], sh[0]-xyi[i][1]+dy[i], coo['label'][i], ha='center', va='top', color=col, fontsize=8)
     
-    labels = 'bcdefg'
+    labels = 'bcdefghijk'
     
-    for j, i in enumerate([6,5,4]):
-        ax = fig.add_subplot(gs[j,3])
+    iters = [6,5,4]
+    if ny == 4:
+        iters += [7]
+        
+    for j, i in enumerate(iters):
+
+        if i == 7:
+            col = 'pink'
+        else:
+            col = 'w'
+
+        ax = fig.add_subplot(gs[j,ny])
         img = plt.imread('{0}.on{1}.png'.format(root, coo['label'][i]))
         ax.imshow(img, origin='upper')
         ax.axis('off')
         
         ax.text(0.05, 0.95, '{0}): {1}'.format(labels[j], coo['label'][i]), 
-                ha='left', va='top', color='w', transform=ax.transAxes)
+                ha='left', va='top', color=col, transform=ax.transAxes)
         
         di = 0.02
         if j == 2:
@@ -125,13 +151,13 @@ def rgb():
             ax.text(0.06+di, 0.18, '2016', ha='left', va='bottom', color='w',
                     transform=ax.transAxes)
             
-        ax = fig.add_subplot(gs[j,4])
+        ax = fig.add_subplot(gs[j,ny+1])
         img = plt.imread('{0}.off{1}.png'.format(root, coo['label'][i]))
         ax.imshow(img, origin='upper')
         ax.axis('off')
         
-        ax.text(0.05, 0.95, '{0})'.format(labels[j+3], coo['label'][i]), 
-                ha='left', va='top', color='w', transform=ax.transAxes)
+        ax.text(0.05, 0.95, '{0})'.format(labels[j+ny], coo['label'][i]), 
+                ha='left', va='top', color=col, transform=ax.transAxes)
         
         if j == 2:
             ax.text(0.06+di, 0.07, r'$y_\mathrm{{110}}$', ha='left', va='bottom', color='w', transform=ax.transAxes)
@@ -140,12 +166,96 @@ def rgb():
             
             ax.text(0.06+di, 0.18, '2019', ha='left', va='bottom', color='w',
                     transform=ax.transAxes)
-                            
+    
+    if 1:
+        # Error ellipse for SN4
+        thet = np.linspace(0, 2*np.pi, 256)
+        xt = np.cos(thet)*4.95*pixgrow
+        yt = np.sin(thet)*1.14*pixgrow
+        phi = (-126.5)/180*np.pi
+        _mat = np.array([[np.cos(phi), -np.sin(phi)], [np.sin(phi), np.cos(phi)]])
+        xyreg = np.dot(np.array([xt, yt]).T, _mat)
+        xreg = xyreg[:,0] + xyi0[-1,0]
+        yreg = sh[0]-(xyreg[:,1] + xyi0[-1,1])
+        fig.axes[0].plot(xreg, yreg, color=col)
+        
+        # Subregion
+        pixgrow2 = img.shape[0]/2/N
+        xt = np.cos(thet)*4.95
+        yt = np.sin(thet)*1.14
+        phi = (-126.5)/180*np.pi
+        _mat = np.array([[np.cos(phi), -np.sin(phi)], [np.sin(phi), np.cos(phi)]])
+        xyreg = np.dot(np.array([xt, yt]).T, _mat)
+        xreg = (xyreg[:,0] + xy0[7,0] - xy[7][0] + N)*pixgrow2
+        yreg = img.shape[0] - (xyreg[:,1] + xy0[7,1] - xy[7][1] + N)*pixgrow2
+        
+        for a in fig.axes[-2:]:
+            a.plot(xreg, yreg, color=col)
+        
     gs.tight_layout(fig, pad=0.1)
     fig.savefig('fig1_layout.pdf', dpi=150)   
     plt.close('all')
         
+def phot_limits():
+    """
+    Compute photometric limits in circular apertures
+    """
+    import sep
     
+    rd = np.array([(24.51007534, -21.92734181),
+          (24.51231984, -21.92978758),
+          (24.51512533, -21.93066599)])
     
-    
+    filts = {}
+    for filt in ['f110w','f125w','f140w','f160w']:
+        sci = pyfits.open(f'j013804m2156-{filt}_drz_sci.fits')
         
+        wht = pyfits.open(f'j013804m2156-{filt}_drz_wht.fits')
+        wcs = pywcs.WCS(sci[0].header)
+        
+        psf = pyfits.open(f'j013804m2156-{filt}_psf.fits')
+        
+        filts[filt] = {'im':sci, 'sci':sci[0].data.byteswap().newbyteorder(), 'wht':wht[0].data.byteswap().newbyteorder(), 'wcs':wcs, 'psf':psf['PSF','DRIZ1'].data.byteswap().newbyteorder()}
+        
+    # Bottom image
+    
+    xpi = filts['f140w']['wcs'].all_world2pix(rd, 0)
+    xi = np.cast[int](np.round(xpi))
+    N = 10
+        
+    filt = 'f140w'
+    i = 2
+    xi[2][1] -= 4
+    xi[0][0] -= 4
+    
+    for filt in ['f110w','f125w','f140w']:
+        slx = slice(xi[i][0]-N, xi[i][0]+N)
+        sly = slice(xi[i][1]-N, xi[i][1]+N)
+    
+        subsci = filts[filt]['sci'][sly, slx]
+    
+        yprof = subsci.mean(axis=1)
+        xprof = (subsci.T-yprof).T.mean(axis=0)
+    
+        yp, xp = np.indices(subsci.shape)-N
+        _A = [subsci.flatten()*0.+1]
+        order = 5
+        for j in range(1,order+1):
+            if i == 2:
+                _A.append(yp.flatten()**j)
+            else:
+                _A.append(xp.flatten()**j)
+    
+        _A = np.array(_A).T
+        _c = np.linalg.lstsq(_A, subsci.flatten(), rcond=-1)
+        _m = _A.dot(_c[0]).reshape(subsci.shape)
+    
+        var = 1/filts[filt]['wht'][sly, slx]
+        xc = xpi-xi+N
+        _phot = sep.sum_circle((subsci - _m).astype(np.float32), [xc[i,0]], [xc[i,1]], [3.5], subpix=0, var=var)
+    
+        ZP, ee = get_hst_ee(filt) 
+        print('{0} {1:.2f}±{2:.2f}  {3:.2f} {4:.2f} {5:.2f}'.format(filt, _phot[0][0], _phot[1][0], ZP - 2.5*np.log10(_phot[0][0]/ee), 2.5/np.log(10)*_phot[1][0]/_phot[0][0], ZP - 2.5*np.log10(_phot[1][0]/ee*5)))
+    
+    
+    
